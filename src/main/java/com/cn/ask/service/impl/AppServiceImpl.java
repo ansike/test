4a374c6e88f71f0a17488281fde0432d6763f54a
ansike <984315765@qq.com>
2017-03-09 10:11:17 +0800

    书城boy开始

--- src/main/java/com/cn/ask/service/impl/AppServiceImpl.java
+++ src/main/java/com/cn/ask/service/impl/AppServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cn.ask.service.impl;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,16 +11,19 @@ import com.cn.ask.model.AppExample;
 import com.cn.ask.service.AppService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+
 @Service
 public class AppServiceImpl implements AppService {
 
-	@Autowired AppMapper appMapper;
+	@Autowired
+	AppMapper appMapper;
+
 	@Override
 	public PageInfo<App> findApp(Integer start, Integer size, String appName) {
-		String name=appName==null?appName="":appName;
+		String name = appName == null ? appName = "" : appName;
 		PageHelper.startPage(start, size);
-		AppExample example=new AppExample();
-		example.createCriteria().andAppNameLike("%"+name+"%");
+		AppExample example = new AppExample();
+		example.createCriteria().andAppNameLike("%" + name + "%");
 		example.clear();
 		return new PageInfo<App>(appMapper.selectByExample(example));
 	}
@@ -41,8 +46,34 @@ public class AppServiceImpl implements AppService {
 
 	}
 
+	public static void main(String[] args) {
+        int a[] = new int[]{8,1,4,6,2,3,5,7,9};  
+        quickSort(a,0,a.length -1);  
+        System.out.println(Arrays.toString(a));  
 
+	}
 
-
-
+    private static int partition(int data[],int low,int high){//分治  
+        int key = data[low];  
+        while(low<high){  
+            while(low<high && data[high]>key)//从右向左  
+                high--;  
+                data[low] = data[high];  
+                  
+            while(low<high && data[low]<key)//从左向右  
+                low++;  
+                data[high] = data[low];  
+        }  
+        data[low] = key;//把轴元素放在轴所在地位置  
+        return low;//返回轴所在的位置  
+    }  
+      
+    private static void quickSort(int data[],int low,int high){//递归  
+        int q;  
+        if(low<high){  
+             q = partition(data,low,high);  
+             quickSort(data,q+1,high);  
+             quickSort(data,low,q-1);  
+    }  
+    }  
 }
