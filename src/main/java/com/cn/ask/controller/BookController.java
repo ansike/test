53b7dbef11f4daf284a4519ba816e1ad682d8e73
ansike <984315765@qq.com>
2017-03-09 22:31:05 +0800

    分类样式提交

--- src/main/java/com/cn/ask/controller/BookController.java
+++ src/main/java/com/cn/ask/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.cn.ask.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
